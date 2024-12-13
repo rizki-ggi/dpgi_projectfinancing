2a6ccf90cf691b62077ae34f39b7c35dabfa155a
rizki-ggi <165998304+rizki-ggi@users.noreply.github.com>
2024-12-13 14:18:28 +0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,20 +1,23 @@
 import streamlit as st
 
+# Function to check the password
 def check_password():
-    def password_entered():
-        if st.session_state["password"] == "ggi2025":
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # Remove the password from session state
-        else:
-            st.session_state["password_correct"] = False
-
     if "password_correct" not in st.session_state:
-        # Enter the password
-        st.text_input("Password", type="password", on_change=password_entered, key="password")
-        return False
-    elif not st.session_state["password_correct"]:
-        st.text_input("Password", type="password", on_change=password_entered, key="password")
-        st.error("Incorrect password.")
-        return False
-    else:
-        return True
+        st.session_state["password_correct"] = False
+
+    with st.form("login_form"):
+        st.text_input("Password", type="password", key="password")
+        submit_button = st.form_submit_button(label="Submit")
+
+        if submit_button:
+            if st.session_state["password"] == "ggi2025":
+                st.session_state["password_correct"] = True
+                del st.session_state["password"]  # Remove password from session state
+            else:
+                st.error("Incorrect password.")
+
+    return st.session_state["password_correct"]
+
+# Function to sign out
+def sign_out():
+    st.session_state["password_correct"] = False
