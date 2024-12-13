024aab8f494d1f67c4496a4cf3df314a3e4bde04
rizki-ggi <165998304+rizki-ggi@users.noreply.github.com>
2024-12-13 13:55:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from auth import check_password
 
 if check_password():
     st.subheader("Dashboard from Looker Studio")
-    looker_studio_url = "your_looker_embeded_URL"
+    looker_studio_url = "https://lookerstudio.google.com/embed/reporting/7b165fb5-3c3e-4429-b162-a4dbe6438e28/page/xFVvD"
     components.iframe(looker_studio_url, width=800, height=600)
