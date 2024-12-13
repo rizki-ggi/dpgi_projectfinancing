51316b640d0b3ea0064d88f0eae4c9fc7915469a
rizki-ggi <165998304+rizki-ggi@users.noreply.github.com>
2024-12-13 14:19:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import streamlit as st
 import streamlit.components.v1 as components
-from auth import check_password
+from auth import check_password, sign_out
 
-if check_password():
-    st.subheader("Dashboard from Looker Studio")
-    looker_studio_url = "https://lookerstudio.google.com/embed/reporting/7b165fb5-3c3e-4429-b162-a4dbe6438e28/page/xFVvD"
-    components.iframe(looker_studio_url, width=800, height=600)
+if not check_password():
+    st.stop()
+
+st.subheader("Dashboard from Looker Studio")
+
+# Add Sign Out button
+if st.button("Sign Out"):
+    sign_out()
+    st.experimental_rerun()
+
+# URL to embed Looker Studio
+looker_studio_url = "https://lookerstudio.google.com/embed/reporting/7b165fb5-3c3e-4429-b162-a4dbe6438e28/page/xFVvD"
+components.iframe(looker_studio_url, width=800, height=600)
