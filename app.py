3708a0ebabf4175d227c7b98955b0dff2df2ffe8
rizki-ggi <165998304+rizki-ggi@users.noreply.github.com>
2024-12-13 15:00:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
 import streamlit as st
 import streamlit.components.v1 as components
-from auth import check_password, sign_out
+import streamlit_authenticator as stauth
 
-# Check password
-if not check_password():
-    st.stop()
+# Setup user authentication
+passwords = ["ggi2025"]
 
-# Display the dashboard
-st.subheader("Dashboard from Looker Studio")
-looker_studio_url = "https://lookerstudio.google.com/embed/reporting/7b165fb5-3c3e-4429-b162-a4dbe6438e28/page/xFVvD"
-components.iframe(looker_studio_url, width=2000, height=2000)
+hashed_passwords = stauth.Hasher(passwords).generate()
 
-# Add Sign Out button below the dashboard
-st.write("")  # Add a spacer
-if st.button("Sign Out"):
-    sign_out()
-    st.experimental_rerun()
+# Create a dummy username and name to fit the required parameters
+authenticator = stauth.Authenticate(["user"], ["username"], hashed_passwords, "some_cookie_name", "some_signature_key", cookie_expiry_days=1)
+
+# Simplify login by not showing username field
+name, authentication_status, _ = authenticator.login("Login", "main", username_placeholder="", password_placeholder="Enter Password", hide_usernames=True)
+
+if authentication_status:
+    st.subheader("Dashboard from Looker Studio")
+    looker_studio_url = "https://lookerstudio.google.com/embed/reporting/7b165fb5-3c3e-4429-b162-a4dbe6438e28/page/xFVvD"
+    components.iframe(looker_studio_url, width=800, height=600)
+
+    if st.button("Sign Out"):
+        authenticator.logout("Sign Out", "main")
+        st.experimental_rerun()
+elif authentication_status == False:
+    st.error("Password is incorrect")
+else:
+    st.warning("Please enter your password")
