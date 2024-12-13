3593fc50a0ea2198b253cf91803bb3dbd62392a0
rizki-ggi <165998304+rizki-ggi@users.noreply.github.com>
2024-12-13 14:21:34 +0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -5,16 +5,17 @@ def check_password():
     if "password_correct" not in st.session_state:
         st.session_state["password_correct"] = False
 
-    with st.form("login_form"):
-        st.text_input("Password", type="password", key="password")
-        submit_button = st.form_submit_button(label="Submit")
+    if not st.session_state["password_correct"]:
+        with st.form("login_form"):
+            st.text_input("Password", type="password", key="password")
+            submit_button = st.form_submit_button(label="Submit")
 
-        if submit_button:
-            if st.session_state["password"] == "ggi2025":
-                st.session_state["password_correct"] = True
-                del st.session_state["password"]  # Remove password from session state
-            else:
-                st.error("Incorrect password.")
+            if submit_button:
+                if st.session_state["password"] == "ggi2025":
+                    st.session_state["password_correct"] = True
+                    del st.session_state["password"]  # Remove password from session state
+                else:
+                    st.error("Incorrect password.")
 
     return st.session_state["password_correct"]
 
